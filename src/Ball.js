eea0a7aff0e35f2ebdc36fb5d9bce910dd88060b
Band Ly <bandly93@gmail.com>
2018-02-28 17:23:43 -0800

    move ball class to seperate file

--- src/Ball.js
+++ src/Ball.js
@@ -1,9 +1,70 @@
-
 export default class Ball{
-	constructor(args){
-		this.ballRadius =  args.ballRadius;
-		this.color = args.color;
-		this.x = args.x;
-		this.y = args.y;
+	constructor(){
+		this.x = 300;
+		this.y = 200;
+		this.starting_x = this.x;
+		this.starting_y = this.y;
+		this.fillStyle = 'orange';
+		this.dx = 6;
+		this.dy = 0;
+		this.ballRadius = 10;
+
+		document.addEventListener('keydown',this.keyDownHandler,false);
+		document.addEventListener('keyup',this.keyUpHandler,false);
 	}
+
+	keyDownHandler = (e) => {	
+		if(e.keyCode == 32){
+			this.switchBallDirection();
+		}
+	}
+		
+	switchBallDirection(){
+		if(this.dx === 0){
+			this.dx = 6;
+			this.dy = 0;
+		}else{
+			this.dx = 0;
+			this.dy = 6;
+		}
+	}	
+	
+	draw(context){
+		context.beginPath();
+		context.arc(this.starting_x,this.starting_y,this.ballRadius,0,Math.PI*2);
+		context.fillStyle = this.fillStyle;
+		context.fill();
+		context.closePath();
+	}
+		
+	boundaries = (context) => {
+		//left to right	
+		if(this.x + this.dx > context.canvas.width-this.ballRadius || this.x + this.dx < this. ballRadius) {
+			this.dx = -this.dx;
+		}
+		//up and down
+		if(this.y + this.dy > context.canvas.height-this.ballRadius || this.y + this.dy <this. ballRadius) {
+			//this.dy = -this.dy;
+			this.dy = -this.dy
+		}
+ 	}
+	
+	move(context){
+		context.beginPath();
+		context.arc(this.x,this.y,this.ballRadius,0,Math.PI*2);
+		context.fillStyle = this.fillStyle;
+		context.fill();
+		context.closePath();
+		this.boundaries(context)
+		
+		requestAnimationFrame(()=>this.update(context))
+
+	}
+	update(context){
+		requestAnimationFrame(()=>this.move(context))
+		context.clearRect(0,0,context.canvas.width,context.canvas.height)
+		this.x += this.dx;
+		this.y += this.dy;	
+	}	
 }
+
--- src/EmojiArcade.js
+++ src/EmojiArcade.js
@@ -2,80 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateData } from './redux/GameModule.js';
 import { updateView } from './redux/ViewModule.js';
-
-//import Ball from './Ball.js';
-
-class Ball{
-	constructor(){
-		this.x = 300;
-		this.y = 200;
-		this.starting_x = this.x;
-		this.starting_y = this.y;
-		this.dx = 3;
-		this.dy = 0;
-		this.ballRadius = 10;
-
-		document.addEventListener('keydown',this.keyDownHandler,false);
-		document.addEventListener('keyup',this.keyUpHandler,false);
-	}
-
-
-	keyDownHandler = (e) => {	
-		if(e.keyCode == 32){
-			this.switchBallDirection();
-		}
-	}
-		
-	switchBallDirection(){
-		if(this.dx === 0){
-			this.dx = 3;
-			this.dy = 0;
-		}else{
-			this.dx = 0;
-			this.dy = 6;
-		}
-	}	
-	
-	draw(context){
-		context.beginPath();
-		context.arc(this.starting_x,this.starting_y,this.ballRadius,0,Math.PI*2);
-		context.fillStyle = 'orange';
-		context.fill();
-		context.closePath();
-	}
-		
-	boundaries = (context) => {
-		//left to right	
-		if(this.x + this.dx > context.canvas.width-this.ballRadius || this.x + this.dx < this. ballRadius) {
-			this.dx = -this.dx;
-		}
-		//up and down
-		if(this.y + this.dy > context.canvas.height-this.ballRadius || this.y + this.dy <this. ballRadius) {
-			//this.dy = -this.dy;
-			this.dy = -this.dy
-			this.ballRadius += 10;
-		}
- 	}
-	
-	move(context){
-		context.beginPath();
-		context.arc(this.x,this.y,this.ballRadius,0,Math.PI*2);
-		context.fillStyle = 'orange';
-		context.fill();
-		context.closePath();
-		this.boundaries(context)
-		
-		requestAnimationFrame(()=>this.update(context))
-
-	}
-	update(context){
-		requestAnimationFrame(()=>this.move(context))
-		context.clearRect(0,0,context.canvas.width,context.canvas.height)
-		this.x += this.dx;
-		this.y += this.dy;	
-
-	}	
-}
+import Ball from './Ball.js';
 
 class Game extends Component{
 	//refers to the canvas itself.
@@ -97,13 +24,28 @@ class Game extends Component{
 		window.addEventListener('resize',()=>updateView(window.innerWidth));
 		
 	}
-	
+
 	startGame(){
 		//draw a ball
 		const { context } = this.state;
-		let ball = new Ball();
-		ball.draw(context);
-		ball.update(context);	
+		
+		for(let i = 0; i< 6; i++){
+			let ball = new Ball();
+
+			ball.x = ball.x+ (i*17)
+			ball.y = ball.y + (i * 25 )
+			i % 2 ? ball.fillStyle = 'green': ball.fillStyle = 'orange'
+			
+			func(ball)
+		}	
+			
+		
+		
+		function func(ball){	
+			ball.draw(context)
+			ball.update(context);
+		}
+			
 	}
 		
 	canvas = () => {
